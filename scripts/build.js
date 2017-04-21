73112b7c4509b3ad1a54dabbf895a5bf713b4799
arzyu <arzyu@live.cn>
2017-04-21 10:55:40 +0800

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,13 @@
+import shell from 'shelljs';
+
+const commands = [
+  ['rm', '-rf', './dist'],
+  ['mkdir', '-p', './dist'],
+  ['exec', './node_modules/.bin/babel --out-dir ./dist ./src'],
+  ['chmod', '755', './dist/mq-deployer.js']
+];
+
+commands.forEach((cmd) => {
+  const [program, ...args] = cmd;
+  shell[program](...args);
+});
