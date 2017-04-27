ee565b2fe6ab2d37bd84b6b5bd3ece5557f7ea92
arzyu <arzyu@live.cn>
2017-04-27 11:41:40 +0800

    change project structure

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@ import shell from 'shelljs';
 const commands = [
   ['rm', '-rf', './dist'],
   ['mkdir', '-p', './dist'],
-  ['exec', './node_modules/.bin/babel --out-dir ./dist ./src'],
+  ['exec', './node_modules/.bin/babel --out-dir ./dist ./bin'],
   ['chmod', '755', './dist/mq-deployer.js']
 ];
 
